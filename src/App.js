e04eb85db9c13840dec5b814581db6afc16add4c
Christy Gordon <152463396+gordonchristy@users.noreply.github.com>
2024-03-17 23:28:59 -0500

    add files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,71 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './components/nav';
 
 function App() {
+  const [state, setState] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Login with:', state.email, state.password);
+    
+    setState({
+      email: '',
+      password: '',
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <nav className="navbar">
+            <ul>
+              <li><a href='#'>HOME</a></li>
+              <li><a href='#'>SETTINGS</a></li>
+              <li><a href='#'>HELP</a></li>
+            </ul>
+          </nav>
+        </div>
+        <h3 className= "App-border">Log In</h3>
+        <form onSubmit={handleSubmit}>
+          <div className= "container">
+            <div className="form-control">
+              <label>Username</label>
+              <input
+                type="text"
+                name="email"
+                value={state.email}
+                onChange={handleInputChange}
+              />
+            </div>
+            
+            <div className="form-control">
+              <label>Password</label>
+              <input
+                type="password"
+                name="password"
+                value={state.password}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="form-control">
+              <button type="submit">Login</button>
+            </div>
+           </div> 
+        </form>
       </header>
     </div>
   );
---
+++ src/components/nav.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const nav= () => {
+    return (
+    <div>
+        <li>
+            <link to="/">HOME</link>
+        </li>
+        <li>
+            <link to="/">SETTINGS</link>
+        </li>
+        <li>
+            <link to="/">HELP</link>
+        </li>
+    
+    </div>
+    );
+}
+
+export default nav;
